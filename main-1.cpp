c5ba6d3096a8bbcee6b0fad32530f0e573de7a8d
Eliza Hannon <lizey.hannon@gmail.com>
2022-05-13 10:23:10 +0930

    main file

---
+++ main-1.cpp
@@ -0,0 +1,33 @@
+#include "Game.h"
+#include "Room.h"
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    Game game1;
+    Player player1;
+
+    Room *rooms = game1.getRooms();
+    
+    int numRooms = game1.getNumRooms();
+
+    for(int i = 0; i < numRooms; i++){
+        cout << rooms[i].getRoomName() << endl;
+    }
+
+    cout << *(player1.getAccusation()) << " "<< player1.getLocation() << endl;
+   
+
+   
+    Room room1 = Room("Knife", "Purple", false, "Garden");
+
+    cout << room1.getCharacter() << endl;
+    cout << room1.getWeapon() << endl;
+    cout << room1.getPassage() << endl;
+    cout << room1.getRoomName() << endl;
+    
+
+    return 0;
+}
