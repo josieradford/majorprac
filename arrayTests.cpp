2317b12ab07b891d539d0b0f1a8fbd63ea6ed1c5
Eliza Hannon <lizey.hannon@gmail.com>
2022-05-17 15:42:42 +0930

    map edits

--- arrayTests.cpp
+++ arrayTests.cpp
@@ -5,45 +5,61 @@ using namespace std;
 //TRYING TO CODE A MAP GRAPHIC
 
 int main(){
-    int numRooms = 1;
-    int numRows =7;
-    int numCols = 14;
-    //char *map;
-
-/*
+    int numRooms = 0;
+    int numRows = 0;
+    int numCols = 0;
+    char *map;
+    
+    cin >> numRooms;
     
     switch (numRooms){
         case 1:
             //char map[7][14] = {0};
             numCols = 14;
-            numRows = 7;
-            break;
+            numRows = 7;            
+            
+            map = new char[numCols*numRows];
+            for(int row = 0; row < numRows; row++){
+                for(int col = 0; col < numCols; col++){
+                    if (row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
+                        map[row+col] = '#';
+                    } else {
+                        map[row+col] = ' ';
+                    }
+                }
+            }
+        break;            
 
         case 2:
-            //char map2[7][27] = {0};
+            //char map[7][27] = {0};
             numRows =7;
             numCols = 27;
+
+            map = new char[numCols*numRows];
             for(int row = 0; row < numRows; row++){
                 for(int col = 0; col < numCols; col++){
                     if ( col == 13 || row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
-                        map2[row][col] = '#';
+                        map[row+col] = '#';
                     } else {
-                        map2[row][col] = ' ';
+                        map[row+col] = ' ';
                     }
                 }
             }
             break;
 
         case 3:
-            //char map2[7][40] = {0};
+            //char map[7][40] = {0};
             numCols = 40;
             numRows = 7;
+
+            map = new char[numCols*numRows];
             for(int row = 0; row < numRows; row++){
+                //frame
                 for(int col = 0; col < numCols; col++){
-                    if ( col == 26 || col == 13 || row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
-                        map2[row][col] = '#';
+                    if (row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
+                        map[row+col] = '#';
                     } else {
-                    map2[row][col] = ' ';
+                        map[row+col] = ' ';
                     }
 
                 }
@@ -53,14 +69,15 @@ int main(){
         case 4:
             numRows = 13;
             numCols = 27;
-            //char map2[13][27] = {0};
+            //char map[13][27] = {0};
 
+            map = new char[numCols*numRows];
             for(int row = 0; row < numRows; row++){
                 for(int col = 0; col < numCols; col++){
                     if ( row == 6 ||col == 26 || col == 13 || row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
-                        map2[row][col] = '#';
+                        map[row+col] = '#';
                     } else {
-                        map2[row][col] = ' ';
+                        map[row+col] = ' ';
                      }
             
 
@@ -69,22 +86,26 @@ int main(){
             break;
         
         case 5:
+            break;
         
 
 
     }
-    */
-    
 
+    
+    /*
    numRows = 15;
     numCols = 33;
     char map2[14][33] = {0};
 
             for(int row = 0; row < numRows; row++){
                 for(int col = 0; col < numCols; col++){
-                    if ( row == 6 ||col == 12 || row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
+
+                    //external frame
+                    if (row == 0 || col == 0 || col == numCols-1 || row == numRows-1 ){
                         map2[row][col] = '#';
-                    } else {
+                        //internal frame
+                    } else if (row == 6 && col <= 6 || row == 6 && col ){
                         map2[row][col] = ' ';
                      }
             
@@ -92,16 +113,20 @@ int main(){
                 }
             }
 
- 
 
+        char map[7][14] = {0};
+        numCols = 14;
+        numRows = 7;
+    
+*/
 
     //print map
     for(int row = 0; row < numRows; row++){
         for(int col = 0; col < numCols; col++){
-            cout << map2[row][col];
+            cout << map[row+col];
         }
         cout << endl;
     }
     
-
+    delete[] map;
 }
